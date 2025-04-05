4ed9f2a56c10fa49f6cca3ea0f7294c85a227168
justmfundo-github <justmfundo@gmail.com>
2025-04-05 16:26:24 +0200

    Take a look at howSum and bestSum implementations, familiarise self with their implementations. Re-do Memoisation

--- data_structures_and_algorithms/alvin-zablan/dynamic-programming/part05_bestSum/javascript/bestSumMemoised.js
+++ data_structures_and_algorithms/alvin-zablan/dynamic-programming/part05_bestSum/javascript/bestSumMemoised.js
@@ -1,4 +1,5 @@
-const bestSum = (targetSum, numbers) => {
+const bestSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
   if (targetSum === 0) return [];
   if (targetSum < 0) return null;
 
@@ -6,7 +7,7 @@ const bestSum = (targetSum, numbers) => {
 
   for (let num of numbers) {
     const remainder = targetSum - num;
-    const remainderCombination = bestSum(remainder, numbers);
+    const remainderCombination = bestSum(remainder, numbers, memo);
 
     if (remainderCombination !== null) {
       const combination = [...remainderCombination, num];
@@ -17,6 +18,7 @@ const bestSum = (targetSum, numbers) => {
     }
   }
 
+  memo[targetSum] = shortestCombination;
   return shortestCombination;
 };
 
@@ -31,8 +33,10 @@ const bestSum = (targetSum, numbers) => {
 // time: O(m^2 * n)
 // space: O(m^2)
 
-console.log(bestSum(7, [5, 3, 4, 7])); // [7]
-console.log(bestSum(8, [2, 3, 5])); // [3, 5]
-console.log(bestSum(8, [1, 4, 5])); // [4, 4]
-console.log(bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
-//console.log(bestSum(230, [7, 14, -11]));
+// console.log(bestSum(7, [5, 3, 4, 7])); // [7]
+// console.log(bestSum(8, [2, 3, 5])); // [3, 5]
+// console.log(bestSum(8, [1, 4, 5])); // [4, 4]
+// console.log(bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
+//console.log(bestSum(230, [7, 14, 11]));
+console.log(bestSum(3, [-1, 2, 1])); // This solution doesn't work for negative numbers....
+// Something for you to ponder
